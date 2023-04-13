dc419c31956204cd0ef34693f73c4369bc1aeeac
HAMZAH-DAFIR <dafirhamzah@gmail.com>
2023-04-13 18:00:28 +0200

    form, style upgrades, more ux progress are done

--- resources/views/backend/accounting/project/larabuilder.blade.php
+++ resources/views/backend/accounting/project/larabuilder.blade.php
@@ -45,6 +45,9 @@
     <link rel="stylesheet" href="./../builder/css/iframe.css" />
     <link rel="stylesheet" href="{{asset('backend/assets/builder/css/preloader.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
